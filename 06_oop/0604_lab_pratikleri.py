399f3a32469aee7bdd5682330fa6e6df6874424c
azizbektas <azikocan9023@gmail.com>
2022-05-07 23:43:09 +0300

    070522-2343

---
+++ 06_oop/0604_lab_pratikleri.py
@@ -0,0 +1,22 @@
+"""
+   - Araba Galerisi için mimari oluşturulacaktır.
+   - Galeri de arabaların her birinin marka, model, motor, yıl, renk, yas özelliği var
+     - Marka-Model-Motor-Yıl mecbur girilmek zorundadır,
+     - Renk zorunda değil. Girilmez ise tanımsız renk olarak ataması yapılacaktır.
+     - Aynı zamanda yaş değeri kullanıcıdan alınmayacak
+     - Constructor içinde hesaplama ile atama yapılacaktır. 2019 yılı
+   - Show() fonk var. Show içinde arabanı tüm detayları düzgün biçimde formatlanarak gösterilir.
+"""
+
+
+
+"""
+   - Calisan class
+       - ad,soyad,tc,maas
+       - BilgiVer() -> Kullanıcının bilgileri yazdırılır
+       - ZamYap(tl) -> eğer yapılmak istenen zam miktari, maaşın yarısından fazlaysa
+           emin misin diye sorulur. cevap E ise zam yapılır, H ise bişey yapılmaz.
+   -Sirket class
+       - AlimYap() -> çalışan sınıfından nesneyi gönderip bilgilerini yazıp, işe alım yapılır.
+       - IstenCikar() -> çalışan sınıfından nesneyi gönderip bilgilerini yazıp, işten çıkarılır.
+"""
