94d35030001ebbc8c79c8a1c80771ab6637fcf48
azizbektas <azikocan9023@gmail.com>
2022-05-08 12:13:02 +0300

    080522-1212

--- 05_fonksiyonlar/0516_list_arguments.py
+++ 05_fonksiyonlar/0516_list_arguments.py
@@ -1,12 +1,54 @@
 # region list_arguments_giris
 """
-Argüman olarak → int, float, string, gönderebiliyorum, peki
+Argüman olarak → int, float, string,tuple, gönderebiliyorum, peki
 Argüman olarak → list gönderebilir miyim? Cevabı, Evet
 """
 # endregion
 
 # region ornek
 """
- 
+def ortalama(liste):
+    # print(type(liste))
+    tekSayilarinToplami, tekSayilarinAdedi= 0,0
+    for i in liste:
+        if i%2==1:
+            tekSayilarinAdedi += 1
+            tekSayilarinToplami += i
+    return f"listedeki {tekSayilarinAdedi} adet tek sayının toplamı {tekSayilarinToplami}"
+
+print(ortalama([3,5,7,10,11,12])) 
 """
+
 # endregion
+
+
+from cgi import print_arguments
+import time
+# import datetime
+from datetime import date, datetime
+cTime = time.time()  #epoch time
+# print(cTime)
+#print(datetime.now()) #bugünün tarihi 2022-05-08 11:46:36.894214
+#print(datetime.now().timestamp()) #epoch time
+#print(datetime.fromisoformat("1998-07-29").timestamp()) #epoch time
+
+
+def urunTarihKontrol(liste):
+    # print(type(liste))
+    for i in liste:
+        cTime = time.time()
+        pTime = datetime.fromisoformat(i).timestamp()
+        # print(i, " " , pTime, "  ", cTime)
+        # print((cTime-pTime)/86400)
+        if round((cTime-pTime)/86400)>60:
+            print(i)
+
+
+urunTarihleri = [
+    "2022-03-06",
+    "2022-03-07",
+    "2022-03-08",
+    "2022-03-09",
+    "2022-03-10",
+]
+urunTarihKontrol(urunTarihleri)
--- 05_fonksiyonlar/0517_kargs.py
+++ 05_fonksiyonlar/0517_kargs.py
@@ -1,6 +1,52 @@
 # region kargs_giris
 """
-Argüman olarak → int, float, string, list gönderebiliyorum, peki
+Argüman olarak → int, float, string, tuple, list gönderebiliyorum, peki
 Argüman olarak → dictionary gönderebilir miyim? Cevabı, Evet
 """
 # endregion
+
+
+
+
+
+"""def pDilleri(**kargs):
+    print(type(kargs))
+
+
+pDilleri(
+    programlamaDili="python",
+    seviye = "yüksek",
+    interpterer = True,
+    version = 3.10
+)"""
+
+
+
+
+
+dilPython ={
+    "programlamaDili" :"python",
+    "seviye" : "yüksek",
+    "interpterer" : True,
+    "version" : 3.10
+}
+
+
+dilCSharp ={
+    "programlamaDili" :"c#",
+    "seviye" : "yüksek",
+    "interpterer" : False,
+    "version" : 8.0
+}
+
+
+
+def dilBilgisi(dil):
+    # print(type(dil))
+    print(f"{dil['programlamaDili']}\t\t{dil['seviye']}\t\t{dil['interpterer']}\t\t{dil['version']}")
+
+
+print("Prg.Dili\tSeviye\t\tInterprt.\tVers.")
+print("------\t\t-------\t\t-------\t\t------")
+dilBilgisi(dilPython)
+dilBilgisi(dilCSharp)
