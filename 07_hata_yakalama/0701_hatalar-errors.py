126eda4e868f685951c537224079bea823e9a57c
azizbektas <azikocan9023@gmail.com>
2022-05-22 11:28:44 +0300

    250522-1128

---
+++ 07_hata_yakalama/0701_hatalar-errors.py
@@ -0,0 +1,29 @@
+# region
+"""
+Uygulama Anı Hataları → [+] Kodlama - coding time
+                        [+] Çalışma - runtime
+                        [+] İstisna - exception
+"""
+# endregion
+
+
+# hatayı kodlama anında farketme
+"""a = 10
+ print(a)"""
+
+# hatayı runtime da farketme
+"""a = 10
+def myFun():
+    pass
+
+myFun(a)"""
+
+# exception
+"""a = 0
+def myFun(x):
+    print(10/int(input("lütfen sayı giriniz : ")))
+
+myFun(a)"""
+
+
+# yazılımcıya bir not → BÜTÜN HATALARI ÖN GÖREREK HATALARI KONTROL ETMEN GEREK
