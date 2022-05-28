3aecc746907247611efee29e19333dcd20541b43
azizbektas <azikocan9023@gmail.com>
2022-05-28 11:27:10 +0300

    280522-1127

---
+++ 08_dosya_yonetimi/0801_dosya_islemleri_open_read.py
@@ -0,0 +1,15 @@
+# file I/O
+# region shell-pratikleri
+"""
+Type "help", "copyright", "credits" or "license" for more information.
+>>> f = open("cihazlar.txt")
+>>> f
+<_io.TextIOWrapper name='cihazlar.txt' mode='r' encoding='cp1254'>
+>>> f = open("cihazlar.txt", encoding="utf-8") 
+>>> f
+<_io.TextIOWrapper name='cihazlar.txt' mode='r' encoding='utf-8'>
+>>> f.read()
+'sw1,127.0.0.1'
+>>>
+"""
+# endregion
