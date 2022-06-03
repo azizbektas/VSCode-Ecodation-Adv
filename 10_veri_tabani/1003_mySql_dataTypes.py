6c1138cd46d92cb96ee753342bc1548612281268
azizbektas <azikocan9023@gmail.com>
2022-06-04 02:11:28 +0300

    030622-2030

--- 10_veri_tabani/1003_mySql_dataTypes.py
+++ 10_veri_tabani/1003_mySql_dataTypes.py
@@ -1,5 +1,7 @@
 # region mySql_dataTypes
 """
 https://dev.mysql.com/ → Documentation → MySQL 8.0 Reference → MySQL 8.0 Reference Manual
+yada
+https://www.tutorialspoint.com/mysql/mysql-data-types.htm#
 """
 # endregion
---
+++ 10_veri_tabani/1018_aggregation_functions.py
@@ -0,0 +1,9 @@
+# region agg_fun
+"""
+COUNT()	kaç adet var
+MAX()	max değer
+MIN()	min değer
+SUM()	bir kolondaki toplam değerler
+AVG()	bir kolondaki ortalama değerler
+"""
+# endregion
